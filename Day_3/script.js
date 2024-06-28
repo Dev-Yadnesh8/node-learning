167ea3935eef17fb2ee7a75323813662c1ac0736
Dev-Yadnesh8 <yadnesh.narawade22@gmail.com>
2024-06-28 09:54:41 +0530

    day3 installing packages , uninstalling , installing specific version ,and more

---
+++ Day_3/script.js
@@ -0,0 +1,11 @@
+// installing packages 
+// npm i package_name 
+// uninstall package
+// npm uninstall package_name
+
+// installing specific package version
+// npm i package_name@version 
+
+// dependencies --> packages and packages ki dependenies 
+// devDependencies --> those packages jo sirf development ke time use hogi , and when we deploy app it will not use 
+// npm i package_name --save-dev (used to save package as dev dependiency)
