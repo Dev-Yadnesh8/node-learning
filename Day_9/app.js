dba60e97038b7f2c6c2e689fb151efd79de1f847
Dev-Yadnesh8 <yadnesh.narawade22@gmail.com>
2024-07-18 22:31:25 +0530

    login / logout , jwt  , password encrypting and storing

---
+++ Day_9/app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+const userModel = require('./models/user');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome');
+});
+
+app.get('/create', (req, res) => {
+    let { userName, email, password, age } = req.body;
+    // this will create user and hash it's password using bcrypt
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, (err, hash) => {
+            let createdUser = userModel.createdUser({
+                userName,
+                email,
+                password: hash,
+                age,
+
+            });
+
+            // creating token for user to directly sign in after creating account 
+          let token =   jwt.sign({email},'shhhhhhhhh');
+          res.cookie('token',token);
+        
+
+        });
+
+    });
+});
+
+// logout user 
+// simply clear the token 
+app.post('/logout',function(req,res){
+   res.cookie('token','');
+});
+
+app.post('/login',function(req,res){
+    let user = userModel.findOne({emial : req.body.email});
+    if(!user) res.send('Somthing went wrong!!');
+    bcrypt.compare(req.body.password ,user.password,(err,result)=>{
+        if(!result){
+            res.send('Somthing went wrong!!');
+        }else{
+            let token =   jwt.sign({emai : user.email},'shhhhhhhhh');
+            res.cookie('token',token);
+            res.send('You can login ');
+        }
+    });
+ });
+
+app.listen(3000);
